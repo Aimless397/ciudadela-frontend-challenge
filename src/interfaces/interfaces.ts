0c0a5c296f1fa861fceed8075dc987839bb603b4
Douglas Zuñiga <douglaszs397@gmail.com>
2023-10-05 16:17:26 -0500

    feat: :sparkles: add Interfaces to improve readability in TS

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,35 @@
+export interface CharactersResponse {
+  fetchCharacters: FetchCharacters
+}
+
+export interface FetchCharacters {
+  info: Info
+  results: Result[]
+}
+
+export interface Info {
+  count: number
+  pages: number
+  next: string
+  prev: string
+}
+
+export interface Result {
+  id: number
+  name: string
+  status: string
+  species: string
+  type: string
+  gender: string
+  origin: Location
+  location: Location
+  image: string
+  episode: string[]
+  url: string
+  created: string
+}
+
+export interface Location {
+  name: string
+  url: string
+}
